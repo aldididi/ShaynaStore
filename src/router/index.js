83dc445a21e2e23df80a212fd0e07a32436e7bda
aldididi <aldyh45@gmail.com>
2020-11-05 21:26:52 +0700

    convert all  html to vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import products from '../views/Products.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +20,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  }, {
+    path: '/products',
+    name: 'products',
+    component: products
+  },
+  {
+    path: '/Cart',
+    name: 'cart',
+    component: ShoppingCart
+  },
+  {
+    path: '/Success',
+    name: 'Success',
+    component: Success
+  },
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
     <HeaderShayna />
+    <HeroShayna />
+    <WomenBanner />
+    <InstaShayna />
+    <PartnerLogo />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import Footer from "@/components/FooterShayna.vue";
 
 export default {
   name: "Home",
   components: {
     HeaderShayna,
+    HeroShayna,
+    WomenBanner,
+    InstaShayna,
+    PartnerLogo,
+    Footer,
   },
 };
 </script>
