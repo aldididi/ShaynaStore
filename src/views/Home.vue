91fdff2f015621f21586c8da1d4101afeb125fa1
aldididi <aldyh45@gmail.com>
2020-11-05 15:01:40 +0700

    first commit shayna

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+  },
+};
+</script>
